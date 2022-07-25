008541d0e4918b20823bbb823e37a8128ed5cdc7
szinuccsi <44683763+szinuccsi@users.noreply.github.com>
2022-07-25 16:02:14 +0200

    Interpolator1D wip

--- src/torch_bwim/interpolators/Interpolator1DFunction.py
+++ src/torch_bwim/interpolators/Interpolator1DFunction.py
@@ -1,44 +1,49 @@
+from typing import Optional
+
 import torch
 from torch.autograd import Function
 
 
 class Interpolator1DFunction(Function):
 
-    '''
-        kernel_xs: shape(kernel_size) sorted
-        kernel: shape(batch, kernel_size)
-        xs: shape(kernel_size)
-        fill_value: 'edge' or constant
-    '''
     @staticmethod
-    def forward(ctx, xs, ctrl_xs, ctrl_values, method='linear', fill_value='edge'):
-        ctx.save_for_backward(xs)
+    def forward(ctx, x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor,
+                left: Optional[torch.Tensor], right: Optional[torch.Tensor],
+                grad_fp: torch.Tensor) -> torch.Tensor:
+        ctx.save_for_backward(x)
+        ctx.grad_fp = grad_fp
 
-        indices = torch.searchsorted(ctrl_xs, xs, right=True)
-
+        indices = torch.searchsorted(xp, x, right=True)
         indices = [
-            torch.clamp(indices - 1.0, min=0, max=len(ctrl_xs) - 1).int(),
-            torch.clamp(indices, min=0, max=len(ctrl_xs) - 1).int()
+            torch.clamp(indices - 1.0, min=0, max=len(xp) - 1).int(),
+            torch.clamp(indices, min=0, max=len(xp) - 1).int()
         ]
 
-        w_interp = [torch.abs(torch.index_select(ctrl_xs, dim=0, index=indices[1 - i]) - xs) for i in range(2)]
-        w_interp = Interpolator1DFunction._normalize_w_interp(w_interp=w_interp)
-        f_values = [torch.index_select(ctrl_values, dim=1, index=indices[i]) for i in range(2)]
+        w_interp = [torch.abs(torch.index_select(xp, dim=0, index=indices[1 - i]) - x) for i in range(2)]
+        w_interp = Interpolator1DFunction.normalize_w_interp(w_interp=w_interp)
+        fp_values = [torch.index_select(fp, dim=0, index=indices[i]) for i in range(2)]
 
-        output = Interpolator1DFunction._interpolate(w_interp=w_interp, f_values=f_values)
-        output = Interpolator1DFunction._out_of_bounds_interpolate(
-            xs=xs, kernel_xs=ctrl_xs, values=output, fill_value=fill_value
+        output = w_interp[0] * fp_values[0] + w_interp[1] * fp_values[1]
+        output = Interpolator1DFunction.out_of_bounds_interpolate(
+            x=x, f=output, xp=xp, left=left, right=right
         )
         ctx.indices = indices
         ctx.w_interp = w_interp
+        ctx.grad_fp = grad_fp
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        pass
+        indices = ctx.indices
+        w_interp = ctx.w_interp
+        grad_fp = ctx.grad_fp
+
+        grad_fp_values = [torch.index_select(grad_fp, dim=0, index=indices[i]) for i in range(2)]
+        output = w_interp[0] * grad_fp_values[0] + w_interp[1] * grad_fp_values[1]
+        return grad_output * output, None, None, None, None, None
 
     @staticmethod
-    def _normalize_w_interp(w_interp):
+    def normalize_w_interp(w_interp):
         sum_w = w_interp[0] + w_interp[1]
         w_interp = [w_interp[i] / sum_w for i in range(2)]
         nans = [torch.isnan(w_interp[i]) for i in range(2)]
@@ -47,14 +52,11 @@ class Interpolator1DFunction(Function):
         return w_interp
 
     @staticmethod
-    def _interpolate(w_interp, f_values):
-        return w_interp[0] * f_values[0] + w_interp[1] * f_values[1]
-
-    @staticmethod
-    def _out_of_bounds_interpolate(xs, kernel_xs, values, fill_value):
-        if fill_value == 'edge':
-            return values
-        min_x, max_x = torch.min(kernel_xs), torch.max(kernel_xs)
-        values = torch.where(xs < min_x, torch.full_like(values, fill_value=fill_value), values)
-        values = torch.where(max_x < xs, torch.full_like(values, fill_value=fill_value), values)
-        return values
+    def out_of_bounds_interpolate(x: torch.Tensor, f: torch.Tensor, xp: torch.Tensor,
+                                  left: Optional[torch.Tensor], right: Optional[torch.Tensor]):
+        min_x, max_x = torch.min(xp), torch.max(xp)
+        if left is not None:
+            f = torch.where(x < min_x, torch.full_like(f, fill_value=left), f)
+        if right is not None:
+            f = torch.where(max_x < x, torch.full_like(f, fill_value=right), f)
+        return f
---
+++ test/test_Interpolator1D.py
@@ -0,0 +1,129 @@
+import datetime
+import unittest
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch_bwim.dataset.TorchDataUtils import TorchDataUtils
+from torch_bwim.interpolators.Interpolator1D import Interpolator1D
+from torch_bwim.interpolators.Interpolator1DFunction import Interpolator1DFunction
+from torch_bwim.nets.NnModuleUtils import NnModuleUtils
+
+
+class Interpolator1DTestCase(unittest.TestCase):
+
+    FIRST_SECTION = (-3.5, 1.5)
+    FIRST_NUM = 100
+    SECOND_SECTION = (2.0, 10.0)
+    SECOND_NUM = 81
+
+    TEST_SECTION = (-4.0, 12.0)
+    TEST_NUM = 128
+
+    xp: np.ndarray
+    fp: np.ndarray
+
+    epsilon = 1e-2
+
+    def setUp(self):
+        self.xp, self.fp = self.xp_fp_create()
+        self.torch_interpolator = Interpolator1D.from_numpy(xp=self.xp, fp=self.fp)
+
+    def xp_fp_create(self):
+        first_section = np.linspace(start=self.FIRST_SECTION[0], stop=self.FIRST_SECTION[1], num=self.FIRST_NUM)
+        second_section = np.linspace(start=self.SECOND_SECTION[0], stop=self.SECOND_SECTION[1], num=self.SECOND_NUM,
+                                     endpoint=True)
+        xp = np.concatenate((first_section, second_section))
+        fp = np.concatenate((
+            50.0 * np.sin(first_section * 10.0),
+            np.square(second_section)
+        ))
+        self.grad_fp = np.gradient(fp, xp)
+        return xp, fp
+
+    def test_forward_one_seq(self):
+        x = self.benchmark_section_create()
+        np_f = np.interp(x, xp=self.xp, fp=self.fp)
+        torch_f = self.torch_interpolator.forward(NnModuleUtils.from_array(x))
+        TorchDataUtils.check_shape(torch_f, expected_shape=x.shape)
+        plt.plot(x, np_f, label='numpy')
+        plt.plot(x, torch_f.numpy(), label='torch')
+        plt.legend()
+        plt.show()
+
+        exp_f = np_f.tolist()
+        act_f = torch_f.detach().numpy().tolist()
+        for i in range(len(act_f)):
+            self.assertAlmostEqual(act_f[i], exp_f[i], delta=self.epsilon)
+
+    def benchmark_section_create(self):
+        return np.linspace(start=self.TEST_SECTION[0], stop=self.TEST_SECTION[1], num=self.TEST_NUM)
+
+    def test_forward_more_seqs(self):
+        x = np.asarray([
+            self.benchmark_section_create(),
+            0.5 * self.benchmark_section_create()
+        ])
+        np_f = np.asarray([np.interp(x[i], xp=self.xp, fp=self.fp) for i in range(x.shape[0])])
+
+        torch_f = self.torch_interpolator.forward(NnModuleUtils.from_array(x))
+        TorchDataUtils.check_shape(torch_f, expected_shape=x.shape)
+
+        exp_f = np_f
+        act_f = torch_f.detach().numpy()
+        for i in range(act_f.shape[0]):
+            for j in range(act_f.shape[1]):
+                self.assertAlmostEqual(act_f[i][j], exp_f[i][j], delta=self.epsilon)
+
+    def test_backward_one_seq(self):
+        np_x = self.benchmark_section_create()
+        np_grad_f = np.interp(np_x, xp=self.xp, fp=self.grad_fp)
+
+        torch_x = NnModuleUtils.from_array(np_x)
+        torch_x.requires_grad = True
+        torch_f = self.torch_interpolator.forward(torch_x)
+        loss = torch.sum(torch_f, dim=0)
+        loss.backward()
+
+        torch_grad_f = torch_x.grad
+
+        plt.plot(np_x, np_grad_f, label='numpy')
+        plt.plot(np_x, torch_grad_f.numpy(), label='torch')
+        plt.legend()
+        plt.show()
+
+        exp_f = np_grad_f.tolist()
+        act_f = torch_grad_f.detach().numpy().tolist()
+        for i in range(len(act_f)):
+            self.assertAlmostEqual(act_f[i], exp_f[i], delta=self.epsilon)
+
+    def test_backward_more_seq(self):
+        np_x = np.asarray([
+            self.benchmark_section_create(),
+            self.benchmark_section_create()
+        ])
+        np_grad_f = np.asarray([np.interp(np_x[i], xp=self.xp, fp=self.grad_fp)
+                                for i in range(np_x.shape[0])])
+
+        torch_x = NnModuleUtils.from_array(np_x)
+        torch_x.requires_grad = True
+        torch_f = self.torch_interpolator.forward(torch_x)
+        loss = torch.sum(torch.sum(torch_f, dim=0), dim=1)
+        loss.backward()
+
+        torch_grad_f = torch_x.grad
+
+        plt.plot(np_x, np_grad_f, label='numpy')
+        plt.plot(np_x, torch_grad_f.numpy(), label='torch')
+        plt.legend()
+        plt.show()
+
+        exp_f = np_grad_f.tolist()
+        act_f = torch_grad_f.detach().numpy().tolist()
+        for i in range(len(act_f)):
+            self.assertAlmostEqual(act_f[i], exp_f[i], delta=self.epsilon)
+
+
+if __name__ == '__main__':
+    unittest.main()
