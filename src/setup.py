b1d5809f5b8c03a1910088a67a8dfe8cbe5bd737
szinuccsi <44683763+szinuccsi@users.noreply.github.com>
2022-07-27 19:29:57 +0200

    LICENSE added

---
+++ src/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name="YOUR PACKAGE NAME",
+    version="0.0.1",
+    author="Bence Szinyéri",
+    author_email="szinyeribence@edu.bme.hu",
+    description="Neural Network Framework",
+    long_description="A Python implementation framework to work with neural networks, implemented in PyTorch.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "torch",
+        "matplotlib",
+        "scipy"
+    ],
+    python_requires='>=3.8',
+)
