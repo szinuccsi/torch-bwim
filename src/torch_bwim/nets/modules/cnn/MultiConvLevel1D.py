8f8ea809abfb578f69987c1b3cabb0883aec2619
cserik <61547757+cserik@users.noreply.github.com>
2022-09-21 18:24:45 +0200

    cut out camel cases
    cut out camel cases

--- src/torch_bwim/nets/modules/cnn/MultiConvLevel1D.py
+++ src/torch_bwim/nets/modules/cnn/MultiConvLevel1D.py
@@ -7,49 +7,49 @@ from torch_bwim.nets.modules.cnn.ConvBlock1D import ConvBlock1D
 
 class MultiConvLevel1D(NetBase):
     class Config(NetBase.Config):
-        def __init__(self, inCh, oCh, startFeat, kernelSize, numOfLevels, layersPerLevel, nLinType, dropout_p,
-                     bNorm=False):
+        def __init__(self, in_ch, o_ch, start_feat, kernel_size, num_of_levels, layers_per_level, n_lin_type, dropout_p,
+                     b_norm=False):
             super().__init__()
-            self.inCh = inCh
-            self.oCh = oCh
-            self.startFeat = startFeat
-            self.kernelSize = kernelSize
-            self.numOfLevels = numOfLevels
-            self.layersPerLevel = layersPerLevel
-            self.nLinType = nLinType
+            self.in_ch = in_ch
+            self.o_ch = o_ch
+            self.start_feat = start_feat
+            self.kernel_size = kernel_size
+            self.num_of_levels = num_of_levels
+            self.layers_per_level = layers_per_level
+            self.n_lin_type = n_lin_type
             self.dropout_p = dropout_p
-            self.bNorm = bNorm
+            self.b_norm = b_norm
 
     def __init__(self, config: Config):
         super().__init__(config=config)
         self.config=config
         this = self.config
-        inCh = this.inCh
-        oCh=round(this.startFeat)
+        in_ch = this.in_ch
+        oCh=round(this.start_feat)
 
-        self.featLayer = ConvBlock1D(in_ch=inCh, out_ch=oCh, kernel_size=this.kernelSize,
-                                        activation_function=this.nLinType, dropout_p=this.dropout_p)
-        inCh = oCh
+        self.featLayer = ConvBlock1D(in_ch=in_ch, out_ch=oCh, kernel_size=this.kernel_size,
+                                        activation_function=this.n_lin_type, dropout_p=this.dropout_p)
+        in_ch = oCh
         self.upChannelLayers = nn.ModuleList()
         self.convLayersInLevel = nn.ModuleList()
-        for level in range(this.numOfLevels):
-            oCh = inCh * 2
-            self.upChannelLayers.append(ConvBlock1D(in_ch=inCh, out_ch=oCh, kernel_size=this.kernelSize,
-                                        activation_function=this.nLinType, dropout_p=this.dropout_p))
-            inCh = oCh
+        for level in range(this.num_of_levels):
+            oCh = in_ch * 2
+            self.upChannelLayers.append(ConvBlock1D(in_ch=in_ch, out_ch=oCh, kernel_size=this.kernel_size,
+                                        activation_function=this.n_lin_type, dropout_p=this.dropout_p))
+            in_ch = oCh
             layers = []
-            for l_in_level in range(1, this.layersPerLevel):
-                layers.append(ConvBlock1D(in_ch=inCh, out_ch=oCh, kernel_size=this.kernelSize,
-                                        activation_function=this.nLinType, dropout_p=this.dropout_p))
+            for l_in_level in range(1, this.layers_per_level):
+                layers.append(ConvBlock1D(in_ch=in_ch, out_ch=oCh, kernel_size=this.kernel_size,
+                                        activation_function=this.n_lin_type, dropout_p=this.dropout_p))
             self.convLayersInLevel.append(nn.Sequential(*layers))
-        oCh = this.oCh
-        self.downChannelLayer = ConvBlock1D(in_ch=inCh, out_ch=oCh, kernel_size=this.kernelSize,
-                                        activation_function=this.nLinType, dropout_p=this.dropout_p)
+        oCh = this.o_ch
+        self.downChannelLayer = ConvBlock1D(in_ch=in_ch, out_ch=oCh, kernel_size=this.kernel_size,
+                                        activation_function=this.n_lin_type, dropout_p=this.dropout_p)
 
     def forward(self, input):
         this = self.config
         input = self.featLayer(input)
-        for i in range(this.numOfLevels):
+        for i in range(this.num_of_levels):
             input = self.upChannelLayers[i](input)
             residual_plus = input
             input = self.convLayersInLevel[i](input)
